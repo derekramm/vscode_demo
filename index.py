fbe5d1b08d7685ef10660ef08be0a71f84c27eac
derek_ramm <derek_ramm@icloud.com>
2019-08-14 16:31:42 +0800

    edit
    Signed-off-by: derek_ramm <derek_ramm@icloud.com>

--- index.py
+++ index.py
@@ -1,18 +1,26 @@
 import handout
-doc = handout.Handout('handout_output/index','index')
-"""
-
----
-
-# index.py
-"""
-
-print('hello, world!')
+doc = handout.Handout('output/index','index')
 
 """
+## 第01题
 
-domkn @ 2019-08-13
+数据类型有哪些？
 
----
+Python中的类型有
+
+* int
+* float
+* complex
+* bool
+
+比如int存储的是整型，下面的代码：
 """
+
+age = 18
+
+"""
+
+"""
+
+
 doc.show()
